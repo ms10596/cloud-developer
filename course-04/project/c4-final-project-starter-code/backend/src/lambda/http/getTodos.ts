cd9879adf58ce55843748e949ae5cfa18495d6a2
ms10596 <ms10596@gmail.com>
2020-01-01 15:56:27 +0200

    feat: retrieve elements from dynamodb

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,26 @@
-import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const AWS = require('aws-sdk')
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODO_TABLE
+
+exports.handler = async (event) => {
+  console.log('Processing event: ', event)
+
+  const result = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
